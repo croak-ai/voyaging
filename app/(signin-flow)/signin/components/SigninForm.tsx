5eda3b6d5e4e3afda98e4101f2846a61d9f9abae
Walshington <nicholaswalsh77@gmail.com>
2023-11-30 16:14:20 -0500

    cleanup

--- app/(signin-flow)/signin/components/SigninForm.tsx
+++ app/(signin-flow)/signin/components/SigninForm.tsx
@@ -1,28 +1,28 @@
-'use client';
+"use client";
 
-import * as React from 'react';
+import * as React from "react";
 
-import { Alert, AlertDescription } from '@/components/ui/alert';
+import { Alert, AlertDescription } from "@/components/ui/alert";
 // import { signIn, useSession, getSession } from 'next-auth/react';
-import { useRouter, useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from "next/navigation";
 
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-import { Card, CardContent, CardTitle } from '@/components/ui/card';
+import { Card, CardContent, CardTitle } from "@/components/ui/card";
 //import { firaSansLogo } from '@/utils/fonts';
 
-import { Button } from '@/components/ui/button';
-import { Icons } from '@/components/ui/icons';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { useEffect } from 'react';
-import { toast } from '@/components/ui/use-toast';
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/ui/icons";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useEffect } from "react";
+import { toast } from "@/components/ui/use-toast";
 // import GitHubEduError from './info/github-edu-error';
 // import GitHubEduMessage from './info/github-edu-info';
 // import DemoModeInfo from './info/demo-mode-info';
 // import TempAdminInfo from './info/temp-admin-info';
-import { AreYouSureDialog } from '@/components/general/are-you-sure-alert-dialog';
-import Loading from '@/components/general/loading';
+import { AreYouSureDialog } from "@/components/general/are-you-sure-alert-dialog";
+import Loading from "@/components/general/loading";
 // import GenerateTemporaryAdmin from './generate-temporary-admin';
 import {
   Select,
@@ -30,8 +30,8 @@ import {
   SelectGroup,
   SelectItem,
   SelectTrigger,
-  SelectValue
-} from '@/components/ui/select';
+  SelectValue,
+} from "@/components/ui/select";
 //import FirstTimeLogin from './first-time-login';
 
 //import { demoAccounts } from '@/utils/globalVariables';
@@ -54,9 +54,9 @@ export default function SignInForm() {
   const [isLoading, setIsLoading] = React.useState<{ [key: string]: boolean }>(
     {}
   );
-//   const { data: session } = useSession();
-  const [username, setUsername] = React.useState<string>('');
-  const [password, setPassword] = React.useState<string>('');
+  //   const { data: session } = useSession();
+  const [username, setUsername] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
   const [errorState, setErrorState] = React.useState<Error | null>(null);
 
   if (errorState) {
@@ -64,74 +64,74 @@ export default function SignInForm() {
   }
 
   const callbackUrl = searchParams
-    ? searchParams.get('callbackUrl') || '/'
-    : '/';
+    ? searchParams.get("callbackUrl") || "/"
+    : "/";
 
-  const errorType = searchParams ? searchParams.get('error') : null;
+  const errorType = searchParams ? searchParams.get("error") : null;
   let error: string | null = null;
   if (errorType) {
     switch (errorType) {
-      case 'CredentialsSignin':
-        error = 'Invalid credentials';
+      case "CredentialsSignin":
+        error = "Invalid credentials";
         break;
 
-      case 'Callback':
-        error = 'OAuth Redirect Error Mismatch.';
+      case "Callback":
+        error = "OAuth Redirect Error Mismatch.";
         break;
 
-      case 'OAuthCallback':
+      case "OAuthCallback":
         error = `Third-Party Callback Error.`;
         break;
 
-      case 'username':
-        error = 'Username not found';
+      case "username":
+        error = "Username not found";
         break;
 
-      case 'OAuthSignin':
-        error = 'OAuth Sign In Error.';
+      case "OAuthSignin":
+        error = "OAuth Sign In Error.";
         break;
 
-      case 'OAuthAccountNotLinked':
+      case "OAuthAccountNotLinked":
         error =
-          'There is already an account with that username address and the provider you just tried to sign-in with has Account Linking disabled. ';
+          "There is already an account with that username address and the provider you just tried to sign-in with has Account Linking disabled. ";
         break;
 
       default:
-        error = 'Unknown Error';
+        error = "Unknown Error";
     }
   }
 
-//   useEffect(() =>{
-//     router.refresh()
-//   }, [])
+  //   useEffect(() =>{
+  //     router.refresh()
+  //   }, [])
 
-//   useEffect(() => {
-//     if (error) {
-//       toast({
-//         title: `Error Logging In`,
-//         description: error,
-//         icon: 'error_for_destructive_toasts',
-//         variant: 'destructive'
-//       });
-//     }
-//   }, [error]);
+  //   useEffect(() => {
+  //     if (error) {
+  //       toast({
+  //         title: `Error Logging In`,
+  //         description: error,
+  //         icon: 'error_for_destructive_toasts',
+  //         variant: 'destructive'
+  //       });
+  //     }
+  //   }, [error]);
 
-//   useEffect(() => {
-//     if (session && router) {
-//       router.push('/');
-//     }
-//   }, [session, router]);
+  //   useEffect(() => {
+  //     if (session && router) {
+  //       router.push('/');
+  //     }
+  //   }, [session, router]);
 
   const onCredentialsSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setIsLoading((prevState) => ({ ...prevState, credentials: true })); // Set loading to true at the start of the function
     try {
-        console.log("WE LIT")
-    //   await signIn('credentials', {
-    //     username,
-    //     password,
-    //     callbackUrl: callbackUrl
-    //   });
+      console.log("WE LIT");
+      //   await signIn('credentials', {
+      //     username,
+      //     password,
+      //     callbackUrl: callbackUrl
+      //   });
     } catch (error) {
       setErrorState(error as Error);
     } finally {
@@ -139,62 +139,62 @@ export default function SignInForm() {
     }
   };
 
-//   const onDemoSubmit = async ({ name }: { name: string }) => {
-//     setIsLoading((prevState) => ({ ...prevState, demo: true })); // Set loading to true at the start of the function
-//     try {
-//       await signIn('credentials', {
-//         username: name,
-//         password: name,
-//         demoLogin: true,
-//         callbackUrl: callbackUrl
-//       });
-//     } catch (error) {
-//       setErrorState(error as Error);
-//     } finally {
-//       setIsLoading((prevState) => ({ ...prevState, demo: false })); // Set loading to false at the end of the function
-//     }
-//   };
+  //   const onDemoSubmit = async ({ name }: { name: string }) => {
+  //     setIsLoading((prevState) => ({ ...prevState, demo: true })); // Set loading to true at the start of the function
+  //     try {
+  //       await signIn('credentials', {
+  //         username: name,
+  //         password: name,
+  //         demoLogin: true,
+  //         callbackUrl: callbackUrl
+  //       });
+  //     } catch (error) {
+  //       setErrorState(error as Error);
+  //     } finally {
+  //       setIsLoading((prevState) => ({ ...prevState, demo: false })); // Set loading to false at the end of the function
+  //     }
+  //   };
 
-//   const onOAuthSubmit = async (providerId: string) => {
-//     setIsLoading((prevState) => ({
-//       ...prevState,
-//       [providerId]: true
-//     }));
-//     try {
-//       await signIn(providerId, {
-//         callbackUrl: callbackUrl
-//       });
-//       setIsLoading((prevState) => ({
-//         ...prevState,
-//         [providerId]: false
-//       }));
-//     } catch (error) {
-//       setErrorState(error as Error);
-//     }
-//   };
+  //   const onOAuthSubmit = async (providerId: string) => {
+  //     setIsLoading((prevState) => ({
+  //       ...prevState,
+  //       [providerId]: true
+  //     }));
+  //     try {
+  //       await signIn(providerId, {
+  //         callbackUrl: callbackUrl
+  //       });
+  //       setIsLoading((prevState) => ({
+  //         ...prevState,
+  //         [providerId]: false
+  //       }));
+  //     } catch (error) {
+  //       setErrorState(error as Error);
+  //     }
+  //   };
 
-//   const OAuthButton = ({
-//     provider,
-//     onSubmit
-//   }: {
-//     provider: TProvider;
-//     onSubmit?: (key: string) => Promise<void>;
-//   }) => (
-//     <Button
-//       type="button"
-//       onClick={() => onSubmit && onSubmit(provider.key)}
-//       disabled={Object.values(isLoading).some((value) => value)}
-//     >
-//       {isLoading[provider.key] ? (
-//         <>
-//           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-//           Loading...
-//         </>
-//       ) : (
-//         <>{provider.displayName}</>
-//       )}
-//     </Button>
-//   );
+  //   const OAuthButton = ({
+  //     provider,
+  //     onSubmit
+  //   }: {
+  //     provider: TProvider;
+  //     onSubmit?: (key: string) => Promise<void>;
+  //   }) => (
+  //     <Button
+  //       type="button"
+  //       onClick={() => onSubmit && onSubmit(provider.key)}
+  //       disabled={Object.values(isLoading).some((value) => value)}
+  //     >
+  //       {isLoading[provider.key] ? (
+  //         <>
+  //           <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+  //           Loading...
+  //         </>
+  //       ) : (
+  //         <>{provider.displayName}</>
+  //       )}
+  //     </Button>
+  //   );
 
   return (
     <div className="relative h-screen">
@@ -206,9 +206,7 @@ export default function SignInForm() {
         <CardContent className="flex flex-col w-full">
           <div className="flex justify-between items-center">
             <CardTitle className="text-2xl font-bold font-mono">
-              <span className="text-2xl">
-                Sign in to Voyaging!
-              </span>
+              <span className="text-2xl">Sign in to Voyaging!</span>
             </CardTitle>
           </div>
 
@@ -235,8 +233,8 @@ export default function SignInForm() {
                   </SelectTrigger>
                   <SelectContent>
                     <SelectGroup> */}
-                      {/*<SelectLabel>Admin</SelectLabel>*/}
-                      {/* {demoAccounts.map((account) => (
+            {/*<SelectLabel>Admin</SelectLabel>*/}
+            {/* {demoAccounts.map((account) => (
                         <SelectItem key={account.name} value={account.name}>
                           {`${account.name}@demo.com`}
                         </SelectItem>
@@ -262,71 +260,75 @@ export default function SignInForm() {
               Object.values(providers).some(
                 (provider) => provider.key === 'credentials'
               ) && ( */}
-                <form onSubmit={onCredentialsSubmit}>
-                  <div className="gap-2 space-y-5">
-                    <CardTitle className="text-center text-sm text-muted-foreground">
-                      Enter your username & password below to login
-                    </CardTitle>
+            <form action="/api/auth/sign-in" method="post">
+              <div className="gap-2 space-y-5">
+                <CardTitle className="text-center text-sm text-muted-foreground">
+                  Enter your email & password below to login
+                </CardTitle>
 
-                    <div className="flex flex-col gap-1">
-                      <div className="text-sm font-bold mb-0">Username</div>
+                <div className="flex flex-col gap-1">
+                  <div className="text-sm font-bold mb-0">Username</div>
 
-                      <Label className="sr-only" htmlFor="username">
-                        Username
-                      </Label>
+                  <Label className="sr-only" htmlFor="username">
+                    Email
+                  </Label>
 
-                      <Input
-                        id="username"
-                        name="Username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        placeholder="test@test is a valid username"
-                        type="username"
-                        autoCapitalize="none"
-                        autoComplete="username"
-                        autoCorrect="off"
-                        disabled={isLoading.credentials}
-                        required
-                        className="border flex"
-                      />
-                    </div>
+                  <Input
+                    id="email"
+                    name="email"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    placeholder="test@testing.com is a valid email"
+                    type="email"
+                    autoCapitalize="none"
+                    autoComplete="email"
+                    autoCorrect="off"
+                    disabled={isLoading.credentials}
+                    required
+                    className="border flex"
+                  />
+                </div>
 
-                    <div className="flex flex-col gap-1">
-                      <div className="text-sm font-bold mb-0">Password</div>
-                      <Label className="sr-only" htmlFor="password">
-                        Password
-                      </Label>
-                      <Input
-                        id="password"
-                        name="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        placeholder="test is a valid password"
-                        type="password"
-                        autoCapitalize="none"
-                        autoComplete="password"
-                        autoCorrect="off"
-                        disabled={isLoading.credentials}
-                        required
-                        className="border flex"
-                      />
-                    </div>
-                    <div className="flex flex-col gap-1 mt-1">
-                      {' '}
-                      {/* Added a margin-top class to create a bigger gap */}
-                      <Button
-                        disabled={isLoading.credentials}
-                        variant="outline"
-                      >
-                        {isLoading.credentials && (
-                          <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                        )}
-                        Sign In
-                      </Button>
-                    </div>
-                  </div>
-                </form>
-              {/* )} */}
+                <div className="flex flex-col gap-1">
+                  <div className="text-sm font-bold mb-0">Password</div>
+                  <Label className="sr-only" htmlFor="password">
+                    Password
+                  </Label>
+                  <Input
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="test is a valid password"
+                    type="password"
+                    autoCapitalize="none"
+                    autoComplete="password"
+                    autoCorrect="off"
+                    disabled={isLoading.credentials}
+                    required
+                    className="border flex"
+                  />
+                </div>
+                <div className="flex flex-col gap-1 mt-1">
+                  {" "}
+                  {/* Added a margin-top class to create a bigger gap */}
+                  {/* <Button disabled={isLoading.credentials} variant="outline">
+                    {isLoading.credentials && (
+                      <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+                    )}
+                    Sign In
+                  </Button> */}
+                  <Button variant="outline">Sign In</Button>
+                  <Button
+                    formAction="/api/auth/sign-up"
+                    className="border border-gray-700 rounded px-4 py-2 text-black mb-2"
+                  >
+                    Sign Up
+                  </Button>
+                </div>
+              </div>
+            </form>
+            {/* )} */}
 
             {/* {providers &&
               Object.values(providers).length > 1 &&
