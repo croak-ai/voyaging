a132c5468cbda148f46f0ba9c353c5528745b802
Walshington <nicholaswalsh77@gmail.com>
2023-11-30 14:58:37 -0500

    Name changing

--- app/(signin-flow)/signin/page.tsx
+++ app/(signin-flow)/signin/page.tsx
@@ -1,6 +1,6 @@
 import Messages from "./messages";
 
-export default function Login() {
+export default function Signin() {
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <form
