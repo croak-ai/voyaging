807fa42ac06214b627fa20f688c7779dbfa3e809
Walshington <nicholaswalsh77@gmail.com>
2023-12-05 02:13:08 -0500

    Supabase now working with TRPC

--- trpcServer/routes/bot.ts
+++ trpcServer/routes/bot.ts
@@ -1,6 +1,14 @@
 import { z } from 'zod';
 import { publicProcedure, router } from '../init/trpc';
-import { supabase } from '@/supabase/client';
+//import { supabase } from '@/supabase/client';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import type { Database } from '@/supabase/functions/_lib/database';
+
+//This is huge. This allows uus to call supabase functions from trpc
+//Tomorrow lets maybe package this up better and then put our server in a
+//docker container
+//const supabase = createRouteHandlerClient<Database>({cookies})
  
 export const botRouter = router({
   botUser: publicProcedure
@@ -8,7 +16,11 @@ export const botRouter = router({
     .query(async (opts) => {
       const { input } = opts;
       // Retrieve the user with the given ID
-      //const user = await supabase.auth.getSession();
-      return 'Walsh';
+
+      //Create a function that will call this when invoked.
+      //For some reason calling this funciton too early will cause an async storage bug
+      const supabase = createRouteHandlerClient<Database>({cookies})
+      const user = await supabase.auth.getSession();
+      return user;
     }),
 });
